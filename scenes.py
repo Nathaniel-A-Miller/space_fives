9c0533cf8faa1a7811ff4bd6974d758f8042a92a
Nathaniel Ashton Miller <65291011+Nathaniel-A-Miller@users.noreply.github.com>
2026-02-18 15:39:50 -0700

    Fix syntax error in loss_3 function

--- scenes.py
+++ scenes.py
@@ -245,7 +245,7 @@ def lighthouse_inside():
             st.rerun()
 
 def loss_3():
-    st.write("""
+    st.write("""f
         Captain Grox cackled and pressed a trapdoor button. {char} and all three robots 
         tumbled down a chute and landed in a heap on the cliffs outside.
 
